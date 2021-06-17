0de61f048c6f2cdf48ee3aa6706c2773c4d25bde
Даниил Волнухин <dreamer1899@outlook.com>
2021-06-17 21:10:53 +0300

    o

---
+++ WS/AddEditPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WS
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage()
+        {
+            InitializeComponent();
+            DataContext = _currentHotel;
+            ComboCoutries.ItemsSource = TourBaseEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+              errors.AppenLine("Укажите название отеля");
+        }
+    }
+}
---
+++ WS/HotelPAge.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WS
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelPAge.xaml
+    /// </summary>
+    public partial class HotelPAge : Page
+    {
+        public HotelPAge()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = TourBaseEntities.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate( new AddEditPage());
+        }
+
+        private void BtnEit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void Удалить_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
